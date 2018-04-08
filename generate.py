0e07e41df161e3e5e468db87f0dfb425931e20c3
Connor Claypool <38144163+cclaypool@users.noreply.github.com>
2018-04-09 00:20:24 +0100

    Clarify config_file option help

--- generate.py
+++ generate.py
@@ -9,7 +9,10 @@ def main():
                         using a GRU- or LSTM-based RNN, implemented with PyTorch 
                         [Text generation script]""")
     parser.add_argument("config_file",
-                        help="""File containing the model configuration. See
+                        help="""Path to the python file containing the model
+                             configuration. The .py suffix is optional, and 
+                             since this path will be loaded as a module, it 
+                             must be within the current directory. See 
                              sample_config.py for a commented example""")
     parser.add_argument("-l", "--load-file",
                         help="""Load previously saved model state from LOAD_FILE. 
